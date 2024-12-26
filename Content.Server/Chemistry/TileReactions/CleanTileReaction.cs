6cf993b3da32e24043df1b9f19467ba07176d960
Inorien <Inorien@live.com>
2024-12-26 19:42:05 +0900

    Revert "CleanTileReaction purges all reagents rather than turning them to water"
    This reverts commit 364d459d035c2dd3aa8f8236317e065a7c53da4f.

--- Content.Server/Chemistry/TileReactions/CleanTileReaction.cs
+++ Content.Server/Chemistry/TileReactions/CleanTileReaction.cs
@@ -11,11 +11,26 @@ using System.Linq;
 namespace Content.Server.Chemistry.TileReactions;
 
 /// <summary>
-/// Purges all reagents on a tile
+/// Turns all of the reagents on a puddle into water.
 /// </summary>
 [DataDefinition]
 public sealed partial class CleanTileReaction : ITileReaction
 {
+    /// <summary>
+    /// How much it costs to clean 1 unit of reagent.
+    /// </summary>
+    /// <remarks>
+    /// In terms of space cleaner can clean 1 average puddle per 5 units.
+    /// </remarks>
+    [DataField("cleanCost")]
+    public float CleanAmountMultiplier { get; private set; } = 0.25f;
+
+    /// <summary>
+    /// What reagent to replace the tile conents with.
+    /// </summary>
+    [DataField("reagent", customTypeSerializer: typeof(PrototypeIdSerializer<ReagentPrototype>))]
+    public string ReplacementReagent = "Water";
+
     FixedPoint2 ITileReaction.TileReact(TileRef tile,
         ReagentPrototype reagent,
         FixedPoint2 reactVolume,
@@ -25,6 +40,8 @@ public sealed partial class CleanTileReaction : ITileReaction
         var entities = entityManager.System<EntityLookupSystem>().GetLocalEntitiesIntersecting(tile, 0f).ToArray();
         var puddleQuery = entityManager.GetEntityQuery<PuddleComponent>();
         var solutionContainerSystem = entityManager.System<SharedSolutionContainerSystem>();
+        // Multiply as the amount we can actually purge is higher than the react amount.
+        var purgeAmount = reactVolume / CleanAmountMultiplier;
 
         foreach (var entity in entities)
         {
@@ -34,9 +51,16 @@ public sealed partial class CleanTileReaction : ITileReaction
                 continue;
             }
 
-            solutionContainerSystem.RemoveAllSolution(puddleSolution.Value);
+            var purgeable = solutionContainerSystem.SplitSolutionWithout(puddleSolution.Value, purgeAmount, ReplacementReagent, reagent.ID);
+
+            purgeAmount -= purgeable.Volume;
+
+            solutionContainerSystem.TryAddSolution(puddleSolution.Value, new Solution(ReplacementReagent, purgeable.Volume));
+
+            if (purgeable.Volume <= FixedPoint2.Zero)
+                break;
         }
 
-        return FixedPoint2.Zero;
+        return (reactVolume / CleanAmountMultiplier - purgeAmount) * CleanAmountMultiplier;
     }
 }
